b0f2eb141ff56c73b39683b607e91b8d981514a3
d-oni <wldnjs2516@naver.com>
2020-03-31 10:05:10 +0900

    변

--- src/main/java/team/test/Hello1.java
+++ src/main/java/team/test/Hello1.java
@@ -3,9 +3,27 @@ package team.test;
 public class Hello1 {
 
   public static void main(String[] args) {
-    System.out.println("강사!");
+    System.out.println("강사!222");
+    System.out.println("ET!");
+    System.out.println("ET2!");
+    
+
+    int a = 100;
+    if (a < 100) {
+      System.out.println("오호라... 100 미만!");
+    } else {
+      System.out.println("오호라... 100 이상!");
+    }
+
     System.out.println("하이헬로~~");
     
+    System.out.println("HomeHome");
+    System.out.println("Hello...");
+    System.out.println("It's me...");
+    System.out.println("I was wondering if after all these years you'd like to meet");
+    System.out.println("To go over everything");
+    System.out.println("They say that time's supposed to heal ya");
+
   }
 
 }
