828e3cb9eebc036926989082fd6755f3b58aa5e3
eomjinyoung <jinyoung.eom@gmail.com>
2020-03-31 09:53:05 +0900

    변경: 출력 변경

--- src/main/java/team/test/Hello1.java
+++ src/main/java/team/test/Hello1.java
@@ -3,7 +3,7 @@ package team.test;
 public class Hello1 {
 
   public static void main(String[] args) {
-    System.out.println("강사!");
+    System.out.println("강사!2222");
 
     int a = 100;
     if (a < 100) {
