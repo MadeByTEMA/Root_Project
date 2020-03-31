2cddf17a575901e4ce698852eb09307288dabc19
heusw <TEMA>
2020-03-31 09:53:05 +0900

    commit's

--- src/main/java/team/test/Hello1.java
+++ src/main/java/team/test/Hello1.java
@@ -3,8 +3,10 @@ package team.test;
 public class Hello1 {
 
   public static void main(String[] args) {
-    System.out.println("강사!");
-
+    System.out.println("Hello...");
+    System.out.println("It's me...");
+    System.out.println("I was wondering if after all these years you'd like to meet");
+    System.out.println("To go over everything");
+    System.out.println("They say that time's supposed to heal ya");
   }
-
 }
