aff73c21cb74ec93fce01f394e1c2878969697bf
eomjinyoung <jinyoung.eom@gmail.com>
2020-03-31 09:47:20 +0900

    변경: 조건 추가

--- src/main/java/team/test/Hello1.java
+++ src/main/java/team/test/Hello1.java
@@ -5,6 +5,13 @@ public class Hello1 {
   public static void main(String[] args) {
     System.out.println("강사!");
 
+    int a = 100;
+    if (a < 100) {
+      System.out.println("오호라... 100 미만!");
+    } else {
+      System.out.println("오호라... 100 이상!");
+    }
+
   }
 
 }
