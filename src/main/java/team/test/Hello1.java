fff2c3e1d73d27c60f5a879d34d20c50c2739f51
eomjinyoung <jinyoung.eom@gmail.com>
2020-03-31 09:51:56 +0900

    변경: 서버 코드와 병합

--- src/main/java/team/test/Hello1.java
+++ src/main/java/team/test/Hello1.java
@@ -4,7 +4,6 @@ public class Hello1 {
 
   public static void main(String[] args) {
     System.out.println("강사!");
-    System.out.println("HomeHome");
 
     int a = 100;
     if (a < 100) {
@@ -13,6 +12,7 @@ public class Hello1 {
       System.out.println("오호라... 100 이상!");
     }
 
+    System.out.println("HomeHome");
   }
 
 }
