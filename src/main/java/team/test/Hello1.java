a27948cb6dbcfc804fef4cf933dce76a8f028323
joeunseong <euns29@naver.com>
2020-03-31 10:12:22 +0900

    병합

--- src/main/java/team/test/Hello1.java
+++ src/main/java/team/test/Hello1.java
@@ -7,6 +7,19 @@ public class Hello1 {
     System.out.println("ET!");
     System.out.println("ET2!");
 
+    int a = 100;
+    if (a < 100) {
+      System.out.println("오호라... 100 미만!");
+    } else {
+      System.out.println("오호라... 100 이상!");
+    }
+    System.out.println("Hello...");
+    System.out.println("It's me...");
+    System.out.println("I was wondering if after all these years you'd like to meet");
+    System.out.println("To go over everything");
+    System.out.println("They say that time's supposed to heal ya");
+
     System.out.println("HomeHome");
   }
+
 }
