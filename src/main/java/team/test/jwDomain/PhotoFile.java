e6b2f3aec242337c9069c098c8e13ce7ad7144e5
d-oni <wldnjs2516@naver.com>
2020-04-06 23:24:39 +0900

    [add]도메인 추가

---
+++ src/main/java/team/test/jwDomain/PhotoFile.java
@@ -0,0 +1,12 @@
+package team.test.jwDomain;
+
+import java.util.List;
+
+public class PhotoFile {
+
+  int no;						//사진번호
+  int placeNo;					//장소리뷰번호
+  int infoNo;					//인포번호
+  List<PhotoFile> photoFile;	//사진파일
+
+}
---
+++ src/main/java/team/test/jwDomain/Scarp.java
@@ -0,0 +1,10 @@
+package team.test.jwDomain;
+
+public class Scarp {
+	
+	int no;			//유저번호
+	int dayNo; 		//데이리뷰번호
+	int placeNo;	//장소리뷰번호
+	int infoNo;		//장소인포번호
+
+}
