8e7d11b7ae4c2004c01c385fcbbd1c062449edfd
TEMA <tema_@naver.com>
2020-04-07 19:02:20 +0900

    [del] 필요없는 파일 삭제

--- src/main/java/team/test/SavePoint.java
+++
@@ -1,8 +0,0 @@
-package team.test;
-
-public class SavePoint {
-  public static void main(String[] args) {
-    System.out.println("Sssssssssssssssssssave");
-    System.out.println("쉬 는 시 간");
-  }
-}
